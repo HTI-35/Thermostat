19c24fbd2d661b764caa1d2c0df11d55dc600a2e
Anne Kok <annelindakok@gmail.com>
2015-06-05 14:53:01 +0200

    Day activities
    Draft idea for Monday activity. Other day activities are empty but
    connected

---
+++ DemoThermostat/app/src/main/java/nl/tue/demothermostat/Tuesday.java
@@ -0,0 +1,15 @@
+package nl.tue.demothermostat;
+
+import android.os.Bundle;
+
+/**
+ * Created by Anne on 05/06/2015.
+ */
+public class Tuesday extends Day {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tuesday);
+    }
+}
+
--- DemoThermostat/app/src/main/java/nl/tue/demothermostat/WeekOverview.java
+++ DemoThermostat/app/src/main/java/nl/tue/demothermostat/WeekOverview.java
@@ -1,7 +1,10 @@
 package nl.tue.demothermostat;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Anne on 04/06/2015.
@@ -11,5 +14,55 @@ public class WeekOverview extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weekoverview);
+        Button bMonday = (Button)findViewById(R.id.bMonday);
+        Button bTuesday = (Button)findViewById(R.id.bTuesday);
+        Button bWednesday = (Button)findViewById(R.id.bWednesday);
+        Button bThursday = (Button)findViewById(R.id.bThursday);
+        Button bFriday = (Button)findViewById(R.id.bFriday);
+        Button bSaturday = (Button)findViewById(R.id.bSaturday);
+        Button bSunday = (Button)findViewById(R.id.bSunday);
+
+        bMonday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), Monday.class));
+            }
+        });
+        bTuesday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), Tuesday.class));
+            }
+        });
+        bWednesday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), Wednesday.class));
+            }
+        });
+        bThursday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), Thursday.class));
+            }
+        });
+        bFriday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), Friday.class));
+            }
+        });
+        bSaturday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), Saturday.class));
+            }
+        });
+        bSunday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), Sunday.class));
+            }
+        });
     }
 }
