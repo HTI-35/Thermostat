fbe21ae7b507f1b11d7100b5ffdd5a11d41def1c
jamiro24 <jamiroleander24@gmail.com>
2015-06-20 10:05:46 +0200

    disable landscape
    disabled landscape mode
    found a settext outside mainthread and fixed it

--- DemoThermostat/app/src/main/java/nl/tue/demothermostat/Day.java
+++ DemoThermostat/app/src/main/java/nl/tue/demothermostat/Day.java
@@ -91,8 +91,13 @@ public class Day extends Activity {
                 try {
                     dayTemp = Double.parseDouble(HeatingSystem.get("dayTemperature"));
                     nightTemp = Double.parseDouble(HeatingSystem.get("nightTemperature"));
-                    mondayDayTemp.setText("Day Temperature: " + dayTemp + " \u2103");
-                    mondayNightTemp.setText("Night Temperature: " + nightTemp +  " \u2103");
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mondayDayTemp.setText("Day Temperature: " + dayTemp + " \u2103");
+                            mondayNightTemp.setText("Night Temperature: " + nightTemp +  " \u2103");
+                        }
+                    });
                     wkProgram = HeatingSystem.getWeekProgram();
                     mondaySwitches = wkProgram.getDay("Monday");
                     tuesdaySwitches = wkProgram.getDay("Tuesday");
